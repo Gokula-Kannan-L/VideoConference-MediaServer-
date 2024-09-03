acc33bd2f454ba3875567938aaccd5c1ea9d3317
Gokula Kannan L <64990358+Gokula-Kannan-L@users.noreply.github.com>
2024-09-03 02:32:30 -0700

    Add files via upload

---
+++ src/Layouts/VideoLayout/VideoLayout.tsx
@@ -0,0 +1,49 @@
+import React, { FunctionComponent, ReactNode, useEffect, useRef, useState } from "react";
+import VideoTile, { TileType } from "../../components/VideoTile/VideoTile";
+import { MainTileType, useGlobalState } from "../../GlobalContext/GlobalContext";
+import "./VideoLayout.scss";
+import MainTile from "../../components/MainTile/MainTile";
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+
+const VideoLayout:FunctionComponent = () => {
+    const {meetStateRef, localStream, presenter, isRecording} = useGlobalState();
+    const [tileData, setTileData] = useState<MainTileType | null>(null);
+
+    useEffect(() => {
+        setTileData(presenter);
+    }, [presenter, localStream, meetStateRef.current]);
+
+    return(
+        <div className="video-layout">
+            <div className="local-tile">
+                {meetStateRef.current && localStream &&
+                    <VideoTile
+                        key={meetStateRef.current.currentUser.userKey}
+                        tileType={TileType.local}
+                        width={"370px"}
+                        height={"200px"}
+                        userName={meetStateRef.current.currentUser.userId == meetStateRef.current.host.hostId ? `${meetStateRef.current.currentUser.userName} (Host)` : meetStateRef.current.currentUser.userName}
+                        userKey={meetStateRef.current.currentUser.userKey}
+                        userId={meetStateRef.current.currentUser.userId}
+                        avatarColorCode={meetStateRef.current.avatar}
+                        videoEnabled={meetStateRef.current.currentUser.preference.video}
+                        audioEnabled={false}
+                        screenShareEnabled={false}
+                        videoStream={localStream}
+                        audioStream={localStream}
+                        isPinned={meetStateRef.current.currentUser.isPinned.video}
+                    />
+                }
+            </div>
+            <div className="main-tile">
+               {tileData && <MainTile presenter={tileData}/>}
+            </div>
+            {isRecording && <div className="session-record">
+                <RadioButtonCheckedIcon className="record-icon"/> Recording
+            </div>}
+            
+        </div>
+    )
+}
+
+export default VideoLayout
